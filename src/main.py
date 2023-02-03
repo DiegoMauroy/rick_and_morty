c91ee2ec602b056bec860235524bc3c42ef523c9
DiegoMauroy <diego.mauroy@gmail.com>
2023-02-03 12:01:34 +0100

    get all characters from the API in a dictionnary

--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
+import requests
+
+#### The function returns the data generated from an API call to url_base + query_url ####
+def make_request(url_base, query_url):
+
+    response = requests.get(url_base + query_url)
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+    return data
+
+#### Main ####
 if __name__ == "__main__":
 
-    pass
+    url_base = "https://rickandmortyapi.com/api"
+    query_url = "/character"
+
+    data = make_request(url_base, query_url)
