becb543de9e2eb899d8e3eff841a466cb075f5ec
DiegoMauroy <diego.mauroy@gmail.com>
2023-02-04 13:41:39 +0100

    creation of a python file for functions

--- rick_and_morty/src/main.py
+++ rick_and_morty/src/main.py
@@ -1,38 +1,4 @@
-import requests
-import pandas as pd
-
-#### The function returns the data generated from an API call to url_base + query_url ####
-def make_request(url_base, query_url):
-
-    response = requests.get(url_base + query_url)
-
-    if response.status_code == 200:
-
-        data = response.json()
-
-    return data
-
-#### Transform a dictionary column into several columns (use only if the indexes are unique) ####
-def dictonnary_to_multi_columns(df, col, replace):
-
-    for index, row in df.iterrows():
-
-        for ky, value in row[col].items():
-            
-            df.at[index, col + "_" + ky] = value
-    
-    if replace:
-
-        df = df.drop([col], axis = 1)
-    
-    return df
-
-#### Transform a list column into a string column ####
-def list_to_string(df, col):
-
-    df[col] = [", ".join(row[col]) for index, row in df.iterrows()]
-
-    return df
+from functions import *
 
 #### Main ####
 if __name__ == "__main__":
